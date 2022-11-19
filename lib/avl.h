3d6b916257546403ba79c8dc83eec4094a96f38b
Matheus-lla <113905872+Matheus-lla@users.noreply.github.com>
2022-11-19 13:18:13 -0300

    Update avl.h

--- lib/avl.h
+++ lib/avl.h
@@ -1,14 +1,39 @@
 typedef struct Node {
-   int n;
+   int data;
    int height;
    struct Node *left;
    struct Node *right;
 } Node;
 
 
-/**
- * @brief Cria uma árvore AVL
- * 
- * @return Retorna a árvore criada
- */
-Node *createTree();
+Node *createNode(int data);         /*********************************************/
+
+//int sizeTree(Node *root);
+
+//Node insertNode(Node *root);
+
+//int removeNode(Node *root);
+
+//int searchTree(int data);
+
+//int bfsTree(Node *root)
+
+int height(Node *root);             /*********************************************/
+
+int getBalance(Node *root);         /*********************************************/
+
+void prefix(Node *root);            /*********************************************/
+
+void infix(Node *root);             /*********************************************/
+
+void postfix(Node *root);           /*********************************************/
+
+//void printTree(Node *root)
+
+//void rotateLL();
+
+//void rotateLR();
+
+//void rotateRL();
+
+//void rotateRR();
