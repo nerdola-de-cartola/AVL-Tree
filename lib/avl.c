9366c2e5a40d6dcab754a636b14f52d8a2623dfe
Matheus-lla <113905872+Matheus-lla@users.noreply.github.com>
2022-11-19 13:17:10 -0300

    Update avl.c

--- lib/avl.c
+++ lib/avl.c
@@ -2,11 +2,53 @@
 
 #include "avl.h"
 
-Avl createAvl() {
-   Avl tree;
+Node *createNode(int data){
+    Node *node = (Node *) malloc(sizeof(Node));
 
-   tree.root = NULL;
-   tree.size = 0;
+    node->data = data;
+    node->height = 1;
+    node->left = NULL;
+    node->right = NULL;
 
-   return tree;
+    return node;
+}
+
+int height(Node *root){
+    if(root != NULL){
+        return root->height;
+    }
+
+    return 0;
+}
+
+int getBalance(Node *root){
+    if(root != NULL){
+        return height(root->left) - height(root->right);
+    }
+
+    return 0;
+}
+
+void prefix(Node *root){
+    if(root != NULL){
+        printf("%d ", root->data);
+        prefix(root->left);
+        prefix(root->right);
+    }
+}
+
+void infix(Node *root){
+    if(root != NULL){
+        infix(root->left);
+        printf("%d ", root->data);
+        infix(root->right);
+    }
+}
+
+void postfix(Node *root){
+    if(root != NULL){
+        postfix(root->left);
+        postfix(root->right);
+        printf("%d ", root->data);
+    }
 }
